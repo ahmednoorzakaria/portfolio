2ca1ea756a33702f8b218674a0cde5c3ba77efa5
ahmed <ahmednoorzakaria680@gmail.com>
2024-03-28 19:13:52 +0300

    Finished the portfolio

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,10 +130,12 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_k9a724n', 'template_qznwqg3', form.current, 'QKIvmElfJfvJbv3lY')
       .then((result) => {
         setOpen(true);
         form.current.reset();
+        console.log('Email sent successfully:', result);
+
       }, (error) => {
         console.log(error.text);
       });
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,7 +7,7 @@ const Projects = ({ openModal, setOpenModal }) => {
   return (
     <Container id="projects">
       <Wrapper>
-        <Title>Project</Title>
+        <Title>Projects</Title>
         <Desc>
           I have worked on a wide range of projects. Here are some of my projects.
         </Desc>
